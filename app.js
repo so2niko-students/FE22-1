d8f68df0ca75a2dc858bb213080f617bc4ae5c91
Nick Sotula <so2niko@gmail.com>
2022-05-10 22:35:43 +0300

    j2 update

---
+++ app.js
@@ -0,0 +1,20 @@
+if(7 == 7){
+    console.log("7 это семь");
+}
+//логические операторы - на выходе boolean
+console.log('сравнения', 5 == 5);
+console.log('сравнения', 'cat' == 5);
+console.log('сравнения', '5' == 5);
+console.log('сравнения', 5n == 5);
+console.log('сравнения 0 == false', 0 == false);
+console.log('сравнения "0" == false', '0' == false);
+console.log('сравнения "" == false', '' == false);
+console.log('сравнения null == false', null == false);
+console.log('сравнения null == true', null == true);
+console.log('сравнения undefined == false', undefined == false);
+console.log('сравнения undefined == true', undefined == true);
+
+
+
+
+//
