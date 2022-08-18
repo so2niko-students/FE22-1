7bf5e6f77d20dcde81d37ac50f1cfc4eb191b1fe
Nick <nick@bloodandtreasure.com>
2022-08-18 20:22:09 +0300

    add parts of generate html objects on screen

--- main.js
+++ main.js
@@ -1,50 +1,17 @@
-//1. Выгружаю данные с файла
-const DATA = {
-    URL : './data.json',
-    USER : {}
-};
+const inpDate = document.querySelector('.inp-date');
+const containerKeys = document.querySelector('.keys');
 
-fetch(DATA.URL).then(r => r.json()).then((d) => {
-    DATA.USER = d;
-    renderUserInfo(d);
+inpDate.addEventListener('input', () => {
+    console.log(inpDate.value);
 });
 
-//2. Отрендерить данные на странице
-function renderUserInfo({ name, email, dob, password }){
-    const html = `<div class="name">${ name }</div>
-    <div class="password">${ password }</div>
-    <div class="dob">${ dob }</div>
-    <div class="mail">${ email }</div>`;
+document.body.addEventListener('keypress', ev => {
+    console.log(ev);
+    const html = getHTMLElement(ev.keyCode, ev.key);
+    containerKeys.insertAdjacentHTML('beforeend', html);
 
-    document.querySelector('.info-user').innerHTML = html;
+})
+
+function getHTMLElement(size, text){
+    return `<div style="width: ${size}px; height: ${ size }px">${ text }</div>`;
 }
-
-//3. Отрендерить форму при нажатии на кнопку
-const USER_FORM = {
-    selectors : ['inp-name', 'inp-password', 'inp-dob', 'inp-email']
-}
-
-USER_FORM.elements = USER_FORM.selectors.reduce((acc, s) => {
-    const name = s.split('-').slice(-1);
-    acc[name] = document.querySelector(`.${ s }`);
-    return acc;
-}, {});
-
-const modal = document.querySelector('.modal');
-
-document.querySelector('.btn-change').addEventListener('click', () => {
-    modal.classList.remove('hide');
-    Object.entries(USER_FORM.elements).forEach(([key, val]) => {
-        val.value = DATA.USER[key];
-    });
-});
-
-//4. Сохранение данных после редактирования
-document.querySelector('.btn-save').addEventListener('click', () => {
-    modal.classList.add('hide');
-    Object.entries(USER_FORM.elements).forEach(([key, val]) => {
-        DATA.USER[key] = val.value;
-    });
-
-    renderUserInfo(DATA.USER);
-});
