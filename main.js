162858bdca082cfb5e0a761813fe26598cb87cfa
Nick <so2niko@gmail.com>
2022-11-01 20:58:58 +0200

    cookie

--- main.js
+++ main.js
@@ -1,50 +1,74 @@
-//1. Выгружаю данные с файла
-const DATA = {
-    URL : './data.json',
-    USER : {}
-};
+document.cookie = "username=John DoeQw";
+document.cookie = "username=hahahahaah=ha";
+document.cookie = "n=John Doe";
+document.cookie = "q2=Jo";
 
-fetch(DATA.URL).then(r => r.json()).then((d) => {
-    DATA.USER = d;
-    renderUserInfo(d);
-});
+console.log(document.cookie);
 
-//2. Отрендерить данные на странице
-function renderUserInfo({ name, email, dob, password }){
-    const html = `<div class="name">${ name }</div>
-    <div class="password">${ password }</div>
-    <div class="dob">${ dob }</div>
-    <div class="mail">${ email }</div>`;
 
-    document.querySelector('.info-user').innerHTML = html;
+class Cookie{
+    //Считать Все Куки
+    static all(type = 'object'){
+        const cooArr = document.cookie.split('; ');
+
+        const cookie = type === 'object' ? Cookie.#parseToObj(cooArr) : Cookie.#parseToArr(cooArr);
+
+        return cookie;
+    }
+
+    static #getKeyVal(keyval){
+        const eqInd = keyval.indexOf('=');
+        const key = keyval.slice(0, eqInd);
+        const val = keyval.slice(eqInd + 1);
+
+        return [key, val];
+    }
+
+    static #parseToObj(arr){
+        return arr.reduce((acc, el) => {
+            const [ key, val ] = Cookie.#getKeyVal(el);
+            
+            acc[key] = val;
+            
+            return acc;
+        }, {});
+    }
+    static #parseToArr(arr){
+        return arr.map(Cookie.#getKeyVal);
+    }
+
+    //Считать Куки по ключу
+    static get(key = 'name'){
+        const cooObj = Cookie.all();
+        
+        return cooObj[key];
+    }
+
+    //Создать/перезаписать Куки
+    static set(key, val, exdays = 0){
+        let expires = '';
+        if(exdays){
+            const d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            expires = ";expires="+d.toUTCString();
+        }
+
+        const str = `${ key }=${ val }${ expires }`;
+        document.cookie = str;
+        
+        return true;
+    }
+
+    //Удалить Куки
+    static delete(key){
+        Cookie.set(key, '');
+    }
 }
 
-//3. Отрендерить форму при нажатии на кнопку
-const USER_FORM = {
-    selectors : ['inp-name', 'inp-password', 'inp-dob', 'inp-email']
-}
 
-USER_FORM.elements = USER_FORM.selectors.reduce((acc, s) => {
-    const name = s.split('-').slice(-1);
-    acc[name] = document.querySelector(`.${ s }`);
-    return acc;
-}, {});
-
-const modal = document.querySelector('.modal');
-
-document.querySelector('.btn-change').addEventListener('click', () => {
-    modal.classList.remove('hide');
-    Object.entries(USER_FORM.elements).forEach(([key, val]) => {
-        val.value = DATA.USER[key];
-    });
-});
-
-//4. Сохранение данных после редактирования
-document.querySelector('.btn-save').addEventListener('click', () => {
-    modal.classList.add('hide');
-    Object.entries(USER_FORM.elements).forEach(([key, val]) => {
-        DATA.USER[key] = val.value;
-    });
-
-    renderUserInfo(DATA.USER);
-});
+console.log(Cookie.all());
+console.log(Cookie.all(2323));
+console.log(Cookie.get('name'));
+console.log(Cookie.get('username'));
+console.log(Cookie.get('n'));
+console.log(Cookie.set('n22', 'Hello 22 days', 22));
