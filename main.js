81120d6b41361bf123841dde369c424916089edd
Nick <so2niko@gmail.com>
2022-11-03 20:20:03 +0200

    fetch

--- main.js
+++ main.js
@@ -1,74 +1,31 @@
-document.cookie = "username=John DoeQw";
-document.cookie = "username=hahahahaah=ha";
-document.cookie = "n=John Doe";
-document.cookie = "q2=Jo";
+const link = 'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json';
 
-console.log(document.cookie);
+fetch(link).then(r => r.json()).then(console.log);
 
+const r = await fetch(link);
+const d = await r.json();
+console.log(d);
 
-class Cookie{
-    //Считать Все Куки
-    static all(type = 'object'){
-        const cooArr = document.cookie.split('; ');
-
-        const cookie = type === 'object' ? Cookie.#parseToObj(cooArr) : Cookie.#parseToArr(cooArr);
-
-        return cookie;
-    }
-
-    static #getKeyVal(keyval){
-        const eqInd = keyval.indexOf('=');
-        const key = keyval.slice(0, eqInd);
-        const val = keyval.slice(eqInd + 1);
-
-        return [key, val];
-    }
-
-    static #parseToObj(arr){
-        return arr.reduce((acc, el) => {
-            const [ key, val ] = Cookie.#getKeyVal(el);
-            
-            acc[key] = val;
-            
-            return acc;
-        }, {});
-    }
-    static #parseToArr(arr){
-        return arr.map(Cookie.#getKeyVal);
-    }
-
-    //Считать Куки по ключу
-    static get(key = 'name'){
-        const cooObj = Cookie.all();
-        
-        return cooObj[key];
-    }
-
-    //Создать/перезаписать Куки
-    static set(key, val, exdays = 0){
-        let expires = '';
-        if(exdays){
-            const d = new Date();
-            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-            expires = ";expires=" + d.toUTCString();
+function myF(url){
+    const f =  new Promise((res, rej) => {
+        const aj = new XMLHttpRequest();
+        aj.onload = () => {
+            res(aj.responseText);
         }
+    
+        aj.open('GET', url);
+    
+        aj.send();
+    });    
 
-        const str = `${ key }=${ val }${ expires }`;
-        document.cookie = str;
-        
-        return true;
-    }
-
-    //Удалить Куки
-    static delete(key){
-        Cookie.set(key, '');
-    }
+    return f;
 }
 
+myF(link).then(d => console.log(JSON.parse(d)));
 
-console.log(Cookie.all());
-console.log(Cookie.all(2323));
-console.log(Cookie.get('name'));
-console.log(Cookie.get('username'));
-console.log(Cookie.get('n'));
-console.log(Cookie.set('n22', 'Hello 22 days', 22));
+// $("label[for*='_logxstar_']").parent().find('.gui-field-content').remove();
+
+// const billPC = $('#billing_postcode').val().replace(' ','').toUpperCase();
+// const billPCInput =  $('#billing_postcode');
+// const billPCValue = billPCInput.val();
+// const billPCFormated = billPCValue.replace(' ', '').toUpperCase();
