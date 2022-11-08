a32e57c582c8171027e963bac88e9feea34c8f49
Nick <so2niko@gmail.com>
2022-11-08 20:56:51 +0200

    add tg bot

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-const link = 'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json';
+const url = 'https://api.telegram.org/bot5689472337:AAHG0BXmRws7qqvkbv6AYBe4pFFrnlT4p50/getUpdates';
+let lastID = '';
 
-fetch(link).then(r => r.json()).then(d => {
-    console.log(d);
-});
+setInterval(() => {
+    fetch(url).then(r => r.json()).then(d => render(d));
+}, 1000);
 
-// const r = await fetch(link);
-// const d = await r.json();
-// console.log(d);
+function render(d){
+    const data = d.result;
+    const id = data.slice(-1)[0].update_id;
+    if(id == lastID) return;
+    lastID = id;
+    const container = document.querySelector('.tg-bot-texts');
 
-function myF(url){
-    const f =  new Promise((res, rej) => {
-        const aj = new XMLHttpRequest();
-        aj.onload = () => {
-            res(aj.responseText);
-        }
-    
-        aj.open('GET', url);
-    
-        aj.send();
-    });    
+    const textStr = data.reverse().map((upd) => getRow(upd)).join('');
 
-    return f;
+    container.innerHTML = textStr;
+
 }
 
-myF(link).then(d => console.log(JSON.parse(d)));
-
-// $("label[for*='_logxstar_']").parent().find('.gui-field-content').remove();
-
-// const billPC = $('#billing_postcode').val().replace(' ','').toUpperCase();
-// const billPCInput =  $('#billing_postcode');
-// const billPCValue = billPCInput.val();
-// const billPCFormated = billPCValue.replace(' ', '').toUpperCase();
+function getRow({ update_id, message }){
+    const id = update_id;
+    const text = message.text;
+    const date = message.date;
+    const user =`${ message.from?.first_name || '' } ${ message.from?.last_name || '' }`.trim();
+    return `<tr>
+        <th scope="row">${ id }</th>
+        <td>${ date }</td>
+        <td>${ user }</td>
+        <td>${ text }</td>
+    </tr>`;
+}
